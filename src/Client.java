d70e33ac9ba6fe65500c591d72cce138325abae9
Vigor Turujlija <vigor.turujlija@gmail.com>
2020-01-07 23:58:37 +0100

    Optimized

--- src/Client.java
+++ src/Client.java
@@ -1,10 +1,16 @@
 // Client2 class that
 // sends data and receives also
 
+import java.awt.*;
 import java.io.*;
 import java.net.*;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 class Client {
+
+    static ArrayList<String> sendQueue = new ArrayList<>();
+    static ArrayList<String> receiveQueue = new ArrayList<>();
 
     public static void main(String args[])
             throws Exception
@@ -24,29 +30,94 @@ class Client {
                 new InputStreamReader(
                         s.getInputStream()));
 
+        // start scanners in separate thread to not stop program
+        startScannerIn();
+        startScannerOut(br);
+
         // to read data from the keyboard
-        BufferedReader kb
-                = new BufferedReader(
-                new InputStreamReader(System.in));
-        String str, str1;
+//        BufferedReader kb
+//                = new BufferedReader(
+//                new InputStreamReader(System.in));
+//        String receiveString;
 
         // repeat as long as exit
         // is not typed at client
-        while (!(str = kb.readLine()).equals("exit")) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    mainloop:
+                    while (true) {
 
-            // send to the server
-            dos.writeBytes(str + "\n");
+                        Thread.sleep(100);
 
-            // receive from the server
-            str1 = br.readLine();
+                        // go through list of send queue:
+                        for (String eachString : new ArrayList<>(sendQueue)) {
+                            sendQueue.remove(0);
+                            println(eachString + " is now sent");
+                            // send to the server
+                            dos.writeBytes(eachString + "\n");
+                            if (eachString.toLowerCase().equals("closeconn")) break mainloop;
+                        }
 
-            System.out.println(str1);
-        }
+                        for (String eachString : new ArrayList<>(receiveQueue)) {
+                            receiveQueue.remove(0);
+                            if (eachString != null) {
+                                handleMessage(eachString);
+                                if (eachString.toLowerCase().equals("closeconn")) break mainloop;
+                            }
+                        }
+                    }
 
-        // close connection.
-        dos.close();
-        br.close();
-        kb.close();
-        s.close();
+                    dos.close();
+                    br.close();
+                    s.close();
+                } catch (IOException | InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    static void handleMessage(String message){
+        println(message);
+    }
+
+    static void startScannerOut(BufferedReader br){
+        Thread inputThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+
+                try {
+                    while (true) {
+                        receiveQueue.add(br.readLine());
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        inputThread.start();
+    }
+
+    static void startScannerIn(){
+        Thread inputThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                Scanner scan = new Scanner(System.in);
+                while (true) {
+                    sendQueue.add(scan.nextLine());
+                }
+            }
+        });
+
+        inputThread.start();
+    }
+
+    static void println(String s){
+        System.out.println(s);
     }
 }
--- src/Server.java
+++ src/Server.java
@@ -3,8 +3,13 @@
   
 import java.io.*;
         import java.net.*;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 class Server {
+
+    static ArrayList<String> sendQueue = new ArrayList<>();
+    static ArrayList<String> receiveQueue = new ArrayList<>();
 
     public static void main(String args[])
             throws Exception
@@ -13,52 +18,109 @@ class Server {
         // Create server Socket 
         ServerSocket ss = new ServerSocket(888);
 
-        // connect it to client socket 
+        // connect it to client socket
         Socket s = ss.accept();
         System.out.println("Connection established");
 
-        // to send data to the client 
+        // to send data to the client
         PrintStream ps
                 = new PrintStream(s.getOutputStream());
 
-        // to read data coming from the client 
+        // to read data coming from the client
         BufferedReader br
                 = new BufferedReader(
                 new InputStreamReader(
                         s.getInputStream()));
 
-        // to read data from the keyboard 
-        BufferedReader kb
-                = new BufferedReader(
-                new InputStreamReader(System.in));
+        startScannerIn();
+        startScannerOut(br);
 
-        // server executes continuously 
+        // to read data from the keyboard
+//        BufferedReader kb
+//                = new BufferedReader(
+//                new InputStreamReader(System.in));
+
+        // server executes continuously
         while (true) {
 
-            String str, str1;
 
-            // repeat as long as the client 
-            // does not send a null string 
+            // repeat as long as the client
+            // does not send a null string
 
-            // read from client 
-            while ((str = br.readLine()) != null) {
-                System.out.println(str);
-                str1 = kb.readLine();
+            // read from client
+            mainloop: while (true) {
 
-                // send to client 
-                ps.println(str1);
+                for (String eachString : new ArrayList<>(receiveQueue)) {
+                    receiveQueue.remove(0);
+                    handleMessage(eachString);
+                }
+
+                // go through list of send queue:
+                for (String eachString : new ArrayList<>(sendQueue)) {
+                    sendQueue.remove(0);
+                    println(eachString + " is now sent");
+                    // send to the server
+                    ps.println(eachString);
+                    if (eachString.toLowerCase().equals("closeconn")) break mainloop;
+                }
+
+//                str1 = kb.readLine();
+                // send to client
+//                ps.println(str1);
             }
 
-            // close connection 
+            // close connection
             ps.close();
             br.close();
-            kb.close();
+//            kb.close();
             ss.close();
             s.close();
 
-            // terminate application 
+            // terminate application
             System.exit(0);
 
-        } // end of while 
+        } // end of while
+    }
+
+    static void handleMessage(String message){
+        println(message);
+    }
+
+    static void startScannerOut(BufferedReader br){
+        Thread inputThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+
+                try {
+                    while (true) {
+                        receiveQueue.add(br.readLine());
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        inputThread.start();
+    }
+
+    static void startScannerIn(){
+        Thread inputThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                Scanner scan = new Scanner(System.in);
+                while (true) {
+                    sendQueue.add(scan.nextLine());
+                }
+            }
+        });
+
+        inputThread.start();
+    }
+
+    static void println(String s){
+        System.out.println(s);
     }
 } 
